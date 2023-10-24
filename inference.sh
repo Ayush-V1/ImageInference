59b42d440e267fd8c1c4dd79fc37d1d2e9e50a3a
Ayush Verma <ayush@Ayushs-MacBook-Pro.local>
2023-10-24 19:14:21 +0530

    Image inference codebase

---
+++ inference.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ImageInference
+
+# Install necessary packages
+pip install --upgrade pip
+pip install torch
+pip install -e .
+
+# Start the first server and wait for it to complete
+python3 -m llava.serve.controller --host 0.0.0.0 --port 10000 > log.txt 2>&1
+
+# Start the second server and wait for it to complete
+python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path liuhaotian/llava-v1.5-7b > log2.txt 2>&1
+
+# Set the environment variables
+export GRADIO_SERVER_NAME='0.0.0.0'
+export GRADIO_SERVER_PORT='3000'
+
+# Start the final server in the background with nohup
+nohup python3 -m llava.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload > log3.txt 2>&1 &
